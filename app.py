124c81f3629c9d419b62bccb8c55094273dea9e5
DanielM79 <daniel.mulder.nl@gmail.com>
2025-02-26 01:46:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect("https://chatgpt.com/g/...", code=301)
+    return redirect("https://chatgpt.com/g/g-67a9c0b376d881918b85c637d77761f0-pro-seo-assistant", code=301)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
