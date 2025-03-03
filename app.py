dc2feba70b84d4213379e8b992c0149fcd25d4d6
DanielM79 <daniel.mulder.nl@gmail.com>
2025-03-03 01:44:51 +0100

    Version 0.1 homepage added

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
-from flask import Flask, redirect, request
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
+@app.route('/')
+def home():
+    return render_template("index.html")
+
 @app.route('/<path:path>')
 def catch_all(path):
     # Controleer of de aanvraag van gpt.proseo.tech komt
     if request.host == "gpt.proseo.tech":
         return redirect("https://chatgpt.com/g/g-67a9c0b376d881918b85c637d77761f0-pro-seo-assistant", code=301)
-    
+
     # Alle andere domeinen blijven zoals ze zijn (zoals app.proseo.tech)
     return "This is the Shark App", 200
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
